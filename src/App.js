46283268ad3981ecafec3bb206cae34a3259e521
ninvit <ninvit@gmail.com>
2020-12-27 12:51:44 -0300

    uploading form

--- src/App.js
+++ src/App.js
@@ -12,34 +12,83 @@ import {
 import "./index.css";
 
 export const App = () => {
-  const [containerRef] = useMeasure(); 
+
+  
+  const [containerRef] = useMeasure();
 
   const data = [
-    { timestamp: "1519780251293", start: 4000, stop: 2400 },
-    { timestamp: "1519780251293", start: 3000, stop: 1398 },
-    { timestamp: "1519780260201", stop: 2000, span: 9800 },
-    { timestamp: "1519780260201", stop: 2780, span: 3908 },
+    {
+      timestamp: "1234",
+      uv: 4000,
+      pv: 2400,
+    }
   ];
 
   return (
     <div id="container" ref={containerRef}>
+      <form>
+        <label>
+          Type:
+          <select>
+            <option value="data">data</option>
+            <option value="span">span</option>
+            <option value="start">start</option>
+            <option value="stop">stop</option>
+          </select>
+        </label>
+
+        <label>
+          Timestamp:
+          <input type="text" name="timestamp" />
+        </label>
+
+        <label>
+          OS:
+          <select>
+            <option value="Windows">Windows</option>
+            <option value="Linux">Linux</option>
+            <option value="MacOs">MacOs</option>
+          </select>
+        </label>
+
+        <label>
+          Browser:
+          <select>
+            <option value="Chrome">Chrome</option>
+            <option value="Firefox">Firefox</option>
+            <option value="Edge">Edge</option>
+          </select>
+        </label>
+
+        <label>
+          MaxResponseTime:
+          <input type="text" name="maxResponseTime" />
+        </label>
+
+        <label>
+          MinResponseTime:
+          <input type="text" name="minResponseTime" />
+        </label>
+
+
+        <input type="submit" value="Enviar" />
+      </form>
+
       <h2>Intelie Chart Plotter</h2>
       <br />
       <LineChart
-        width={730}
+        width={1000}
         height={250}
         data={data}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        margin={{ top: 10, right: 30, left: 20, bottom: 5 }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="timestamp" />
         <YAxis />
         <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="start" stroke="#100f20" />
-        <Line type="monotone" dataKey="stop" stroke="#ddd4d8" />
-        <Line type="monotone" dataKey="span" stroke="#aaa4d8" />
-        <Line type="monotone" dataKey="data" stroke="#82ca9d" />
+        <Legend align="right" />
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <Line type="monotone" dataKey="pv" stroke="#20afd8" />
       </LineChart>
     </div>
   );
